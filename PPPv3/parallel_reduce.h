9b15119985a182229ed30cd8454447fb5e2e6dc9
Matheson Mawhinney <mmawhinn@sfu.ca>
2016-10-15 14:12:16 -0700

    fix reduce

--- PPPv3/parallel_reduce.h
+++ PPPv3/parallel_reduce.h
@@ -23,41 +23,39 @@ public:
 	}
 
 	void execute() {
-        PPP_DEBUG_MSG("Split: [start: " + to_string(m_start) + ", end: " + to_string(m_end) + ", grain: " + to_string(m_grainsize) + "]");
+    PPP_DEBUG_MSG("Split: [start: " + to_string(m_start) + ", end: " + to_string(m_end) + ", grain: " + to_string(m_grainsize) + "]");
 		if (m_end - m_start < m_grainsize) {
-            T sum;
-            sum = T(0);
-            for (int i = m_start; i < m_end; i++) {
-              sum = sum + m_array[i];
-            }
-            m_sum = sum;
-            return;
+      T sum;
+      sum = T(0);
+      for (int i = m_start; i < m_end; i++) {
+        sum = sum + m_array[i];
+      }
+      m_sum = sum;
+      return;
 		}
         
-		int64_t diff = (m_end - m_start)/2;
-        int64_t new_start = m_start + diff;
-        int64_t new_end = m_end - diff;
+    int64_t middle = (m_start + m_end) / 2;
         
-        ppp::TaskGroup tg;
-		ReduceTask r1(m_array, m_start, new_start, m_grainsize);
-		ReduceTask r2(m_array, new_end, m_end, m_grainsize);
+    ppp::TaskGroup tg;
+		ReduceTask r1(m_array, m_start, middle, m_grainsize);
+		ReduceTask r2(m_array, middle, m_end, m_grainsize);
 		tg.spawn(r1);
 		tg.spawn(r2);
 		tg.wait();
-        m_sum = r1.m_sum + r2.m_sum;
-        PPP_DEBUG_MSG("SUM: " + to_string(m_sum));
+    m_sum = r1.m_sum + r2.m_sum;
+    PPP_DEBUG_MSG("SUM: " + to_string(m_sum));
 	}
     
-    T get_sum() {
-        return m_sum;
-    }
+  T get_sum() {
+    return m_sum;
+  }
     
 private:
 	T* m_array;
 	int64_t m_start;
 	int64_t m_end;
 	int64_t m_grainsize;
-    T m_sum;
+  T m_sum;
 };
   template <typename T>
   extern inline
@@ -65,15 +63,15 @@ private:
   {
     // ASSIGNMENT: make this parallel via recursive divide and conquer
     assert(end >= start);
-	if (grainsize == 0) {
-		grainsize = (end-start+1) / (get_thread_count()*4);
-	}
+    if (grainsize == 0) {
+		  grainsize = (end-start+1) / (get_thread_count()*4);
+    }
 
-	ppp::ReduceTask<T> t(array, start, end, grainsize);
+    ppp::ReduceTask<T> t(array, start, end, grainsize);
     t.execute();
     T thing = t.get_sum();
     PPP_DEBUG_MSG("SUM: " + to_string(thing));
-	return thing;
+    return thing;
   }
 }
 
